2a1aec725804b4eaece2e25f24bdcfb24d890207
Petko Yovchevski <pyovchevski@gmail.com>
2019-02-25 11:20:34 +0200

    updates

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$config = array();
+$config['name'] = "Vimeo Videos Playlist";
+$config['author'] = "Petko Yovchevski";
+$config['ui'] = true;
+$config['version'] = 0.1;
+$config['position'] = 57;
